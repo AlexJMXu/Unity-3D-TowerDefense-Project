7d26733c1d7f66925d583547d08595142e8adb90
Alexander Xu <ajm.xu@outlook.com>
2016-08-09 15:57:47 +0100

    Implemented Feature - Building

---
+++ Assets/BuildManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+	public static BuildManager instance;
+
+	public GameObject standardTurretPrefab;
+
+	private GameObject turretToBuild;
+
+	void Awake() {
+		if (instance != null) {
+			Debug.LogError("Already another instance of BuildManager in scene.");
+			return;
+		} else {
+			instance = this;
+		}
+	}
+
+	void Start() {
+		turretToBuild = standardTurretPrefab;
+	}
+
+	public GameObject GetTurretToBuild() {
+		return turretToBuild;
+	}
+
+}
