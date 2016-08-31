e8ffc22d07dc349d30e79a442fd41c56cbcc28bf
Alexander Xu <ajm.xu@outlook.com>
2016-08-31 15:10:20 +0100

    Implemented Feature - Shop

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -5,6 +5,7 @@ public class BuildManager : MonoBehaviour {
 	public static BuildManager instance;
 
 	public GameObject standardTurretPrefab;
+	public GameObject anotherTurretPrefab;
 
 	private GameObject turretToBuild;
 
@@ -17,12 +18,13 @@ public class BuildManager : MonoBehaviour {
 		}
 	}
 
-	void Start() {
-		turretToBuild = standardTurretPrefab;
-	}
-
 	public GameObject GetTurretToBuild() {
 		return turretToBuild;
+	}
+
+	public void SetTurretToBuild(GameObject turret) {
+		turretToBuild = turret;
+
 	}
 
 }
