5a6c29541e1bbc7629583d4780f9d90fb2e1f084
Alexander Xu <ajm.xu@outlook.com>
2016-09-09 12:39:10 +0100

    Implemented - Missile

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,6 +5,7 @@ public class Bullet : MonoBehaviour {
 	private Transform target;
 
 	public float speed = 70f;
+	public float explosionRadius = 0f;
 
 	public GameObject impactEffect;
 
@@ -28,12 +29,37 @@ public class Bullet : MonoBehaviour {
 		}
 
 		transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+		transform.LookAt(target);
 	}
 
 	void HitTarget() {
 		GameObject effectIns = (GameObject) Instantiate(impactEffect, transform.position, transform.rotation);
-		Destroy(effectIns, 2f);
+		Destroy(effectIns, 5f);
+
+		if (explosionRadius > 0f) {
+			Explode();
+		} else {
+			Damage(target);
+		}
+
 		Destroy(gameObject);
-		Destroy(target.gameObject);
+	}
+
+	void Explode() {
+		Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+		for (int i = 0; i < colliders.Length; i++) {
+			if (colliders[i].tag == "Enemy") {
+				Damage(colliders[i].transform);
+			}
+		}
+	}
+
+	void Damage(Transform enemy) {
+		Destroy(enemy.gameObject);
+	}
+
+	void OnDrawGizmosSelected() {
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, explosionRadius);
 	}
 }
