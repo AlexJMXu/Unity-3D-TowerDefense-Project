3b06382a4cb39c20ec63ce48b91553ce611d7231
Alexander Xu <ajm.xu@outlook.com>
2016-10-18 01:03:07 +0100

    Implemented - Laser Effect

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -16,6 +16,8 @@ public class Turret : MonoBehaviour {
 	[Header("User Laser")]
 	public bool useLaser = false;
 	public LineRenderer lineRenderer;
+	public ParticleSystem impactEffect;
+	public Light impactLight;
 
 	[Header("Unity Setup Fields")]
 	public string enemyTag = "Enemy";
@@ -34,8 +36,11 @@ public class Turret : MonoBehaviour {
 	void Update () {
 		if (target == null) {
 			if (useLaser) {
-				if (lineRenderer.enabled) 
+				if (lineRenderer.enabled) {
 					lineRenderer.enabled = false;
+					impactEffect.Stop();
+					impactLight.enabled = false;
+				}
 			}
 
 			return;
@@ -72,11 +77,19 @@ public class Turret : MonoBehaviour {
 	}
 
 	private void Laser() {
-		if (!lineRenderer.enabled)
+		if (!lineRenderer.enabled) {
 			lineRenderer.enabled = true;
+			impactEffect.Play();
+			impactLight.enabled = true;
+		}
 
 		lineRenderer.SetPosition(0, firePoint.position);
 		lineRenderer.SetPosition(1, target.position);
+
+		Vector3 dir = firePoint.position - target.position;
+
+		impactEffect.transform.position = target.position + dir.normalized;
+		impactEffect.transform.rotation = Quaternion.LookRotation(dir);
 	}
 
 	private void UpdateTarget() {
